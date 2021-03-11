ffc09f8a1e74b23605c6cb533cc071e317660ac5
Andrea <79329770+anj108@users.noreply.github.com>
2021-03-10 22:37:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 hi = "Hi "
-name = input("What is your name?")
-age = int(input("What is your age?"))
+name = input("What is your name? ")
+age = int(input("What is your age? "))
 
 if age > 90:
     print(hi + name + ". You're old.")
